e84c2b6425f0999b5f8cb7d9ce494ddd6dbfc3e0
abrikos <5422791+abrikos@users.noreply.github.com>
2021-01-29 15:51:51 +0900

    graphs

--- src/pages/home/Chart.js
+++ src/pages/home/Chart.js
@@ -4,10 +4,16 @@ import HighchartsReact from 'highcharts-react-official';
 
 export default function Chart(props) {
     const [data, setData] = useState([])
-    const fields = {
-        temp: {label: 'Температура', color: 'red'},
-        mem: {label: 'Память', color: 'green'},
-        util: {label: 'Показатель', color: 'blue'},
+    const types = {
+        watts: {label: 'Потребляемая мощность (Ватт)', color: 'red', series: ['util'], util: 'Ватт'},
+        cpus: {label: 'Процессоры (%)', color: 'green', series: ['util', 'temp'], util: '%'},
+        gpus: {label: 'Видеоускорители (%)', color: 'blue', series: ['util', 'temp', 'mem'], util: '%'},
+    }
+
+    const labels = {
+        util: types[props.type].util,
+        temp: 'Температура',
+        mem: 'Память',
     }
 
     useEffect(() => {
@@ -15,9 +21,6 @@ export default function Chart(props) {
             .then(setData)
     }, [])
 
-    function adaptData(d){
-        return [d.date, d.util];
-    }
 
     console.log(JSON.stringify(data))
     const options = {
@@ -25,46 +28,34 @@ export default function Chart(props) {
             type: 'column'
         },
         title: {
-            text: props.name
+            text: types[props.type].label
         },
         legend: {
             enabled: true
         },
-        xAxis:{ categories: data.map(d=>d.date)},
-        plotOptions:{
-            bar:{
-                dataLabels:true
+        xAxis: {categories: data.map(d => d.date)},
+        plotOptions: {
+            bar: {
+                dataLabels: true
             },
             series: {
                 dataLabels: {
                     enabled: true,
-                    borderRadius: 2,
-                    y: -10,
+                    //borderRadius: 2,
+                    //y: -10,
                     shape: 'callout'
                 }
             }
         },
-        series: [
-            {
-                color: 'green',
-                pointPlacement: -0.2,
-                data: data.map(d=>d.util),
-                name:'Значение',
+        series: types[props.type].series.map(r => {
+            return {
+                color: r.color,
+                //pointPlacement: -0.2,
+                data: data.map(d => d[r]),
+                name: labels[r],
 
-            },
-            {
-                color: 'red',
-                pointPlacement: -0.2,
-                data: data.map(d=>d.temp),
-                name:'Температура C'
-            },
-            {
-                color: 'blue',
-                pointPlacement: -0.2,
-                data: data.map(d=>d.mem),
-                name:'Память'
             }
-        ]
+        })
     };
 
     return <div className="border m-3">
